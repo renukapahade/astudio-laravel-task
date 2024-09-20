9313b37790e2455ddec17a3d43d94b3ff12478d1
Renuka Pahade <renuka391@gmail.com>
2024-09-20 23:11:27 +0530

    Changed the User model columns and migration file

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,11 +16,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $fillable = ['first_name', 'last_name', 'date_of_birth', 'gender', 'email', 'password'];
 
     /**
      * The attributes that should be hidden for serialization.
